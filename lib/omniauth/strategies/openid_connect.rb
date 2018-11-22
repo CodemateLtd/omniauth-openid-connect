dd097931cb49c129f516cb70b7984395d7708d2a
Juhamatti Niemelä <juhamatti.niemela@codemate.com>
2018-11-22 09:49:58 +0200

    Provide `ui_locales` option in `authorize_url`
    Allows OpenIDProvider to display its authentication flow in requested
    locale.

--- lib/omniauth/strategies/openid_connect.rb
+++ lib/omniauth/strategies/openid_connect.rb
@@ -125,7 +125,8 @@ module OmniAuth
           prompt: options.prompt,
           nonce: (new_nonce if options.send_nonce),
           hd: options.hd,
-          acr_values: Array(options.acr_values).join(" ")
+          acr_values: Array(options.acr_values).join(" "),
+          ui_locales: Array(options.ui_locales).join(" ")
         }
         client.authorization_uri(opts.reject { |_k, v| v.nil? })
       end
